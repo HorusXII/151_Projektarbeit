c429401c2c66c844d6f8a699bc13786afc43d701
SupportGIBM <40485324+SupportGIBM@users.noreply.github.com>
2022-06-08 08:54:57 +0200

    init

---
+++ include/dbconnector.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+// TODO - mit eigener Datenbak verbinden
+$host = 'localhost';
+$username = 'TestUser';
+$password = 'password1234';
+$database = '151_projektarbeit';
+
+// mit Datenbank verbinden
+$mysqli = new mysqli($host, $username, $password, $database);
+
+// Fehlermeldung, falls Verbindung fehl schlägt.
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+}
