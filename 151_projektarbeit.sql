05ed69a108c312dff0349b5d43cff2270115a1cf
SupportGIBM <40485324+SupportGIBM@users.noreply.github.com>
2022-06-08 11:35:25 +0200

    Update 151_projektarbeit.sql
    updated the Database to match our requirements (added image & Admin)

--- 151_projektarbeit.sql
+++ 151_projektarbeit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 08. Jun 2022 um 10:57
+-- Erstellungszeit: 08. Jun 2022 um 11:31
 -- Server-Version: 10.1.22-MariaDB
 -- PHP-Version: 7.1.4
 
@@ -30,19 +30,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `image` blob NOT NULL,
   `firstname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `lastname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `Admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Daten für Tabelle `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `email`) VALUES
-(1, 'Lukas', 'Breiter', 'BreiterL', '$2y$10$EiEKtLTsLyubtAfp1ro0H.L6uNzCXokA9P7dQjytKvgA.U9Z25z/m', 'lukasbreiter@bluewin.ch');
+INSERT INTO `users` (`id`, `image`, `firstname`, `lastname`, `username`, `password`, `email`, `Admin`) VALUES
+(1, '', 'Lukas', 'Breiter', 'BreiterL', '$2y$10$EiEKtLTsLyubtAfp1ro0H.L6uNzCXokA9P7dQjytKvgA.U9Z25z/m', 'lukasbreiter@bluewin.ch', 0),
+(2, '', 'Noah', 'Kaderli', 'BreiterL', '$2y$10$Ypr6e2t8/a2BNK..Qp7ceOvaHVdJNDoYdaooLFWOPR8uFs0pH.Lxm', 'uhsdfosahaoh@gmail.com', 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -62,7 +65,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
